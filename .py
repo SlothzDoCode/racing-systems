2ce111af0b067fd2c61d3236e85e95975e1762a6
SlothzDoCode <146091658+SlothzDoCode@users.noreply.github.com>
2023-11-02 15:13:50 +0000

    Update .py

---
+++ .py
@@ -0,0 +1,18 @@
+import random as ran
+import time as t
+from tkinter import *
+import tkinter as tk
+
+start_window_colour = "#121212"
+start_text_colour = "#FFFFFF"
+start_button_colour = "#404040"
+start_frame_colour = "#282828"
+
+
+main = tk.Tk()
+
+main.config(background=start_window_colour)
+
+
+
+mainloop()
